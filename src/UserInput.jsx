17f30128d3b3b3d1ce7e7f733ca29f89776b878a
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-04-05 16:35:33 +0200

    added handle input change function

--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,11 +1,41 @@
-import Input from './Input'
+import { useState } from "react"
 export default function UserInput(){
+    const [userInput, setUserInput] = useState({
+        initialInvestement: 10000,
+        annualInvestement: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+    function handleChange(inputIdentifier, newValue){
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        })
+    }
     return(
         <div id="user-input">
-            <Input label="initizal investment" />
-            <Input label="annual investment" />
-            <Input label="expected return" />
-            <Input label="duration" type="number" />
+            <div className='input-group'>
+                <p>
+                    <label>Ininital investment</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label>Annual investement</label>
+                    <input type="number" required />
+                </p>
+            </div>
+            <div className='input-group'>
+                <p>
+                    <label>Expected return</label>
+                    <input type="number" required />
+                </p>
+                <p>
+                    <label>Duration</label>
+                    <input type="number" required />
+                </p>
+            </div>
         </div>
     )
 }
